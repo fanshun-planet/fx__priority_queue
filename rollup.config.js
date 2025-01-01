a2052ba362b916076932fe9dcb026c66d7c1df02
fanshun <15153019111@163.com>
2025-01-01 23:46:26 +0800

    feat: 优先队列数据接口的初步实现

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/fx__priority_queue.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/fx__priority_queue.esm.js',
+      format: 'es'
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' })
+  ],
+};
---
+++ src/PriorityQueue.ts
@@ -0,0 +1,126 @@
+/**
+ * 优先队列
+ * @class
+ */
+class PriorityQueue<T = number> {
+  private queue: T[];
+  private compare: (a: T, b: T) => boolean;
+
+  /**
+   * @constructor
+   */
+  constructor(compareFn: (a: T, b: T) => boolean) {
+    this.queue = [];
+    this.compare = compareFn;
+  }
+
+  /**
+   * 获取队列元素数量
+   * @returns
+   */
+  size(): number {
+    return this.queue.length;
+  }
+
+  /**
+   * 队列是否为空
+   * @returns
+   */
+  isEmpty(): boolean {
+    return this.queue.length === 0;
+  }
+
+  /**
+   * 入队，添加元素
+   * @param element
+   */
+  enqueue(element: T) {
+    this.queue.push(element);
+    this.heapifyUp(this.queue.length - 1);
+  }
+
+  /**
+   * 出队，获取（移出）顶部元素
+   * @returns
+   */
+  dequeue(): T | null {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const top = this.queue[0];
+    const tail = this.queue.pop()!;
+    if (!this.isEmpty()) {
+      this.queue[0] = tail;
+      this.heapifyDown(0);
+    }
+
+    return top;
+  }
+
+  /**
+   * 交换元素位置
+   * @param idx1
+   * @param idx2
+   */
+  swap(idx1: number, idx2: number) {
+    const q = this.queue;
+    const x = this.queue[idx1];
+    q[idx1] = q[idx2];
+    q[idx2] = x;
+  }
+
+  /**
+   * 上浮特定的元素（从底至顶调整元素位置）
+   * @param idx
+   */
+  heapifyUp(idx: number) {
+    let parentIdx: number = Math.floor((idx - 1) / 2);
+    while (idx > 0 && this.compare(this.queue[idx], this.queue[parentIdx])) {
+      this.swap(idx, parentIdx);
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+  }
+
+  /**
+   * 从顶至下（下沉），调整元素位置，维护堆性质
+   * @param idx
+   */
+  heapifyDown(idx: number) {
+    const leftChildIdx: number = idx * 2 + 1;
+    const rightChildIdx: number = idx * 2 + 2;
+    const q = this.queue;
+    let moreIdx: number = idx;
+
+    if (leftChildIdx < this.size() && this.compare(q[leftChildIdx], q[moreIdx])) {
+      moreIdx = leftChildIdx;
+    }
+    if (rightChildIdx < this.size() && this.compare(q[rightChildIdx], q[moreIdx])) {
+      moreIdx = rightChildIdx;
+    }
+
+    if (moreIdx !== idx) {
+      this.swap(idx, moreIdx);
+      this.heapifyDown(moreIdx);
+    }
+  }
+
+  /**
+   * 查询队首元素（勿直接修改）
+   * @returns
+   */
+  top(): Readonly<T> | null {
+    return this.isEmpty() ? null : this.queue[0];
+  }
+
+  /**
+   * 序列化字符串用于查看数据
+   * @returns
+   */
+  toString() {
+    return JSON.stringify(this.queue);
+  }
+}
+
+export { PriorityQueue };
